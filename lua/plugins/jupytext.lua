dfaa3fd056f8daf075396300499a7c8c144681e8
Luke-Chesley <lukechesleyfive@gmail.com>
2025-02-17 11:07:09 -0500

    updates

--- lua/plugins/jupytext.lua
+++ lua/plugins/jupytext.lua
@@ -7,17 +7,6 @@ return {
     config = function()
         vim.g.jupytext_fmt = "py" -- Convert notebooks to Python scripts
         vim.g.jupytext_auto_save = 1 -- Auto-sync to .ipynb on save
-
-        -- Delete the .py file when the buffer is closed
-        vim.api.nvim_create_autocmd("BufUnload", {
-            pattern = "*.py",
-            callback = function()
-                local file = vim.fn.expand("<afile>")
-                if vim.fn.filereadable(file) == 1 then
-                    os.execute("rm " .. vim.fn.shellescape(file))
-                end
-            end,
-        })
-    end,
+    end
 }
 
